0c10ce9037ed0b896032bce34327f926ff7a619c
Clem <clemence.fauchet23@gmail.com>
2020-01-24 15:52:09 +0100

    Adding weather container properties

--- src/components/WeatherFetching.js
+++ src/components/WeatherFetching.js
@@ -7,11 +7,52 @@ const api = {
 }
 
 function WeatherFetching() {
+  const dateBuilder = (d) => {
+    let months = [
+      'Janvier',
+      'Février',
+      'Mars',
+      'Avril',
+      'Mai',
+      'Juin',
+      'Juillet',
+      'Août',
+      'Septembre',
+      'Octobre',
+      'Novembre',
+      'December',
+    ]
+    let days = [
+      'Dimanche',
+      'Lundi',
+      'Mardi',
+      'Mercredi',
+      'Jeudi',
+      'Vendredi',
+      'Samedi',
+    ]
+
+    let day = days[d.getDay()]
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+    let numero = d.getDate()
+
+    return `${day} ${numero} ${month} ${year}`
+  }
+
   return (
     <div>
       <main>
         <div className='search-box'>
           <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+        <div className='location-box'>
+          <div className='location'>Paris, France </div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-box'>
+          <div className='temp'>15°C</div>
+          <div className='weather'>Brouillard</div>
         </div>
       </main>
     </div>
