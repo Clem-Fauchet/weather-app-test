7c6ce7caf713f81cbb9f7398eac18e97806cb4f4
Clem <clemence.fauchet23@gmail.com>
2020-01-27 20:43:35 +0100

    Updates

---
+++ precache-manifest.4aa9ca8c18a8c017d4840b380957f068.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e229ae962c8f3eef79f21f30fffdf2",
+    "url": "/Clem-Fauchet/weather-app-test.git/index.html"
+  },
+  {
+    "revision": "6afe1769a16c4a68e531",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/css/main.59eabed0.chunk.css"
+  },
+  {
+    "revision": "e911e8d7f909b2731bc1",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/js/2.39f7daf3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/js/2.39f7daf3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6afe1769a16c4a68e531",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/js/main.61e9273b.chunk.js"
+  },
+  {
+    "revision": "9a0f35f57857063ca6d2",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/js/runtime-main.285d7725.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Clem-Fauchet/weather-app-test.git/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
